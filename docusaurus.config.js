324051f95c784fcdaf5824022d287a9fc113dae5
Carsten Lenz <git@lenz-online.org>
2022-11-19 20:11:02 +0100

    undo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://git.clxe.de',
+  url: 'https://carslen.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
